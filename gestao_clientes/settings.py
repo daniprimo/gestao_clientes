bfc7ef17c67cb5259ff021f9a713eca7679303f5
DLLM <DLLM@gft.com>
2023-03-02 12:37:02 -0300

    feat: login do superuser e configuracao dos diretorios

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gestao_clientes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = 'person_list'
+LOGOUT_REDIRECT_URL = 'person_list'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
