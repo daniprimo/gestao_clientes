234a26c544a2dd4879d19427cb84c82daa57176a
DLLM <DLLM@gft.com>
2023-03-02 19:55:54 -0300

    feat: ajustando um monte de coisa nessa merda-

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from decouple import config
 
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 #}
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
