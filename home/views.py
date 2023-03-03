235655298f022e8bde5c3169478fb765165ca956
DLLM <DLLM@gft.com>
2023-03-03 11:30:12 -0300

    ....

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.decorators.csrf import requires_csrf_token
+
 
 def home(request):
     return render(request, 'home.html')
 
+@requires_csrf_token
 def mylogout(request):
     logout(request)
     return redirect('home')
